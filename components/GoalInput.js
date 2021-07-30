6884bddc4a4b0d833923dba13b31ef919bd7b4df
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-07-30 16:40:49 +0300

    add new visiblty element

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
-import { TextInput, Button, View, StyleSheet } from "react-native";
+import { TextInput, Button, View, StyleSheet, Modal } from "react-native";
 const GoalInput = (props) => {
   const [enteredGoals, setEnterGoal] = useState("");
   const goalInputHandler = (enteredText) => {
     setEnterGoal(enteredText);
   };
   return (
-    <View style={styles.input}>
-      <TextInput
-        placeholder="Add new course"
-        onChangeText={goalInputHandler}
-        value={enteredGoals}
-      />
-      <Button title="Add" onPress={props.onAddGoal.bind(this, enteredGoals)} />
-    </View>
+    <Modal visible={props.visible}>
+      <View style={styles.input}>
+        <TextInput
+          placeholder="Add new course"
+          onChangeText={goalInputHandler}
+          value={enteredGoals}
+        />
+        <Button
+          title="Add"
+          onPress={props.onAddGoal.bind(this, enteredGoals)}
+        />
+      </View>
+    </Modal>
   );
 };
 const styles = StyleSheet.create({
